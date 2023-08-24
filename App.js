cc534840367c1e4e752dffe2e04939a1b359dbd2
Marcelo Victor <lvictor462@gmail.com>
2023-08-24 11:38:45 -0300

    add params e campos

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import params from "./src/Params";
+import Field from "./src/components/Field";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Projeto campo minado!!!</Text>
+        <Text> Tamanho da grade:
+          {params.getRowsAmount()} X {params.getColumnsAmount()} </Text>
+
+          <Field />
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
