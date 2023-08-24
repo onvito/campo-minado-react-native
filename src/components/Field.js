e3cff69788f2b22656c6c17b9a9f8b66e1e3d092
Marcelo Victor <lvictor462@gmail.com>
2023-08-24 13:55:49 -0300

    corrigindo d

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,10 +4,12 @@ import { View, Text, StyleSheet } from "react-native";
 import params from "../Params";
 
 export default (props) => {
+  const { mined, opened, nearMines } = props
+
   const styleField = [styles.field];
   if (styleField.length === 1) styleField.push(styles.regular);
   return (
-    <View style={styleFieldF}>
+    <View style={styleField}>
       <Text> Fala zé </Text>
     </View>
   );
